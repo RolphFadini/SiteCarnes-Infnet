0e289524b7b9a8040ed92b0a054ff1472f457827
Rolph Fadini <rolphfadini@hotmail.com>
2023-06-17 02:42:32 -0300

    final commit for checkbox component

--- checkbox.js
+++ checkbox.js
@@ -4,9 +4,15 @@ const checkboxesOp = document.querySelectorAll(".checkbox-option");
 for(let i = 0; i < checkboxesOp.length; i++) {
     checkboxesOp[i].onclick = function() {
         if(i == 0) {
-            checkboxes[0].classList.toggle("marcado");
-            checkboxes[1].classList.toggle("marcado");
-            checkboxes[2].classList.toggle("marcado");
+            if(checkboxes[0].matches(".marcado")) {
+                checkboxes[0].classList.remove("marcado");
+                checkboxes[1].classList.remove("marcado");
+                checkboxes[2].classList.remove("marcado");
+            } else {
+                checkboxes[0].classList.add("marcado");
+                checkboxes[1].classList.add("marcado");
+                checkboxes[2].classList.add("marcado");
+            }
         } else if(i == 1) {
             checkboxes[1].classList.toggle("marcado");
             if(checkboxes[2].matches(".marcado"))
@@ -28,8 +34,15 @@ checkboxes.forEach(checkbox => {
 for(let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].onclick = function() {
         if(i == 0) {
-            checkboxes[1].classList.toggle("marcado");
-            checkboxes[2].classList.toggle("marcado");
+            if(!checkboxes[0].matches(".marcado")) {
+                checkboxes[0].classList.remove("marcado");
+                checkboxes[1].classList.remove("marcado");
+                checkboxes[2].classList.remove("marcado");
+            } else {
+                checkboxes[0].classList.add("marcado");
+                checkboxes[1].classList.add("marcado");
+                checkboxes[2].classList.add("marcado");
+            }
         } else if(i == 1) {
             if(checkboxes[2].matches(".marcado"))
             checkboxes[0].classList.toggle("marcado");
